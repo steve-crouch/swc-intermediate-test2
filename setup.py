96dd144b5c24ad17fdebb52b10079d78228cc906
Steve Crouch <s.crouch@software.ac.uk>
2020-05-26 12:39:07 +0100

    Trigger CI for real

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="swc-intermediate-test", version='1.0', packages=find_packages())
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,5 +29,16 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
 
+
+@pytest.mark.parametrize(
+    "test, expected", 
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4])
+    ])
+def test_daily_mean(test, expected):
+    """Test parametrization"""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(np.array(expected), daily_mean(np.array(test)))
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
